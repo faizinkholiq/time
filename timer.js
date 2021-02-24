273644ace1644ee9b7fd5d968a6257fab10fa8e6
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-02-24 08:51:21 +0700

    update dom

--- timer.js
+++ timer.js
@@ -25,7 +25,7 @@ const bulan = [
   "Desember"
 ];
 
-const today =
+const hari_ini =
   hari[tanggal.getDay()] +
   ", " +
   tanggal.getDate() +
@@ -34,29 +34,46 @@ const today =
   " " +
   tanggal.getFullYear();
 
-document.getElementById("my-body").onload = function() {
-  document.getElementById("today").innerHTML = today;
+const body = document.getElementById("my-body");
+const greet = document.getElementById("greet");
+const today = document.getElementById("today");
+const jam = document.getElementById("jam");
+const menit = document.getElementById("menit");
+const detik = document.getElementById("detik");
+const dayimg = document.getElementById("day");
+
+body.onload = function() {
+  today.innerHTML = hari_ini;
+  setDay(tanggal.getHours);
   setInterval(waktu, 1000);
 };
 
 function waktu() {
   const now = new Date();
 
-  document.getElementById("jam").innerText = ("0" + now.getHours()).slice(-2);
-  document.getElementById("menit").innerText = ("0" + now.getMinutes()).slice(
-    -2
-  );
-  document.getElementById("detik").innerText = ("0" + now.getSeconds()).slice(
-    -2
-  );
+  jam.innerText = ("0" + now.getHours()).slice(-2);
+  menit.innerText = ("0" + now.getMinutes()).slice(-2);
+  detik.innerText = ("0" + now.getSeconds()).slice(-2);
 
+  setDay(now.getHours);
+}
+
+function setDay(hours) {
   // Change DOM
-  let day = document.getElementById("day").getAttribute("src");
+  let day = dayimg.getAttribute("src").split("/");
+  let msg;
 
-  document.getElementById("my-body").classList = "";
-  if (now.getHours() > 18) {
-    document.getElementById("my-body").classList.add("dark");
+  body.classList = "";
+  if (hours > 18) {
+    msg = "Good Night";
+    day[1] = "moon-plain.svg";
+    body.classList.add("dark");
   } else {
-    document.getElementById("my-body").classList.add("light");
+    msg = "Good Morning";
+    day[1] = "sun-plain.svg";
+    body.classList.add("light");
   }
+
+  dayimg.setAttribute("src", day.join("/"));
+  greet.innerText = msg;
 }
